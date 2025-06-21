c283ebfa436bf2f725ecac6a250fdb607c57977b
Fra-Maraglino <francesco.maraglino@edu.unife.it>
2025-06-21 18:37:05 +0200

    Update page title to 'Ricerca per Autore' in authorBooks.php

--- authorBooks.php
+++ authorBooks.php
@@ -22,7 +22,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Cerca Libro</title>
+        <title>Ricerca per Autore</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link
